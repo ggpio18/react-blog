4183d673702fb0fe3eeae5d91faa1df86c2252f8
ggpio18 <piolo18aranza@gmail.com>
2024-05-23 15:46:46 +0800

    chnge: [23/05] updt: [trending.jsx|devbaseimgurl]

--- src/components/pages/developer/ui/home/Trending.jsx
+++ src/components/pages/developer/ui/home/Trending.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import SectionHeader from './SectionHeader'
 import Card from './Card'
+import Markdown from 'react-markdown';
+import useQueryData from '../../../../custom-hook/useQueryData';
+import { devBaseImgUrl } from '../../../../helpers/functions-general';
 
 const Trending = () => {
 
+  const {
+    isLoading,
+    isFetching,
+    error,
+    data: post,
+  } = useQueryData (
+   "/v1/post", // endpoint
+   "get", // method
+   "post", // key
+   
+  );
  
   return (
     <>
@@ -15,41 +29,23 @@ const Trending = () => {
             <Card/>
             <div className='grid gap-10 md:grid md:grid-cols-2'>
               {/* try */}
+              {!isLoading && post?.data.map(
+                    (item, key) => (
+            <div className="shadow-[4px_2px_10px_5px_rgba(0,0,0,0.1)]  p-5 rounded-2xl" key={key}>
+                    <div className='overflow-hidden rounded-xl h-[250px] mb-5 -mt-10'>
+                        <img src={`${devBaseImgUrl}/${item.post_image}`} alt="" className="w-full object-cover h-[300px] block   transition-transform hover:scale-110" />
+                    </div>
+                <small className='hover:bg-accent bg-stone-600  px-2 py-1 rounded-lg text-white font-bold 
+                    text-xs'>Travel</small>
+                    <h3 className='mt-4 mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
 
-            <div className="shadow-[4px_2px_10px_5px_rgba(0,0,0,0.1)]  p-5 rounded-2xl">
-                    <div className='overflow-hidden rounded-xl h-[250px] mb-5 -mt-10'>
-                        <img src="https://starmometer.com/wp-content/uploads/2021/06/BINI-will-unveil-a-fiercer-look-sound-and-attitude-at-their-grand-two-part-launch-this-June-on-KTX.PH-2.jpg" alt="" className="w-full object-cover h-[300px] block   transition-transform hover:scale-110" />
-                    </div>
-                <small className='hover:bg-accent bg-stone-600  px-2 py-1 rounded-lg text-white font-bold 
-                    text-xs'>Travel</small>
-                    <h3 className='mt-4 mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-            </div>
-            <div className="shadow-[4px_2px_10px_5px_rgba(0,0,0,0.1)]  p-5 rounded-2xl">
-                    <div className='overflow-hidden rounded-xl h-[250px] mb-5 -mt-10'>
-                        <img src="https://starmometer.com/wp-content/uploads/2021/06/BINI-will-unveil-a-fiercer-look-sound-and-attitude-at-their-grand-two-part-launch-this-June-on-KTX.PH-2.jpg" alt="" className="w-full object-cover h-[300px] block   transition-transform hover:scale-110" />
-                    </div>
-                <small className='hover:bg-accent bg-stone-600  px-2 py-1 rounded-lg text-white font-bold 
-                    text-xs'>Travel</small>
-                    <h3 className='mt-4 mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-            </div>
-            <div className="shadow-[4px_2px_10px_5px_rgba(0,0,0,0.1)]  p-5 rounded-2xl">
-                    <div className='overflow-hidden rounded-xl h-[250px] mb-5 -mt-10'>
-                        <img src="https://starmometer.com/wp-content/uploads/2021/06/BINI-will-unveil-a-fiercer-look-sound-and-attitude-at-their-grand-two-part-launch-this-June-on-KTX.PH-2.jpg" alt="" className="w-full object-cover h-[300px] block   transition-transform hover:scale-110" />
-                    </div>
-                <small className='hover:bg-accent bg-stone-600  px-2 py-1 rounded-lg text-white font-bold 
-                    text-xs'>Travel</small>
-                    <h3 className='mt-4 mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-            </div>
-            <div className="shadow-[4px_2px_10px_5px_rgba(0,0,0,0.1)]  p-5 rounded-2xl">
-                    <div className='overflow-hidden rounded-xl h-[250px] mb-5 -mt-10'>
-                        <img src="https://starmometer.com/wp-content/uploads/2021/06/BINI-will-unveil-a-fiercer-look-sound-and-attitude-at-their-grand-two-part-launch-this-June-on-KTX.PH-2.jpg" alt="" className="w-full object-cover h-[300px] block   transition-transform hover:scale-110" />
-                    </div>
-                <small className='hover:bg-accent bg-stone-600  px-2 py-1 rounded-lg text-white font-bold 
-                    text-xs'>Travel</small>
-                    <h3 className='mt-4 mb-0'>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-            </div>
-          
+                    <Markdown>{item.post_article}</Markdown>
 
+
+            </div>
+             )
+            ) } 
+           
             </div>
 
           </div>
